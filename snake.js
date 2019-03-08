b098a8fedc35ddebe4798d34fcd4a80e7388913b
Karthik Siddarth <iamkarthiksiddarth@gmail.com>
2019-03-08 20:37:43 +0530

    set canvas body color

--- snake.js
+++ snake.js
@@ -1,5 +1,4 @@
-const CANVAS_BORDER_COLOR = '#000000'
-const CANVAS_BACKGROUND_COLOR = '#FFFFFF'
+const CANVAS_BACKGROUND_COLOR = '#fafafa'
 
 const gameCanvas = document.getElementById('gameCanvas')
 const ctx = gameCanvas.getContext('2d')
