ab8485212986ef965309fa54ee2c3cb98df96a6c
Karthik Siddarth <iamkarthiksiddarth@gmail.com>
2019-03-08 20:38:09 +0530

    fill canvas body with defined color

--- snake.js
+++ snake.js
@@ -2,3 +2,7 @@ const CANVAS_BACKGROUND_COLOR = '#fafafa'
 
 const gameCanvas = document.getElementById('gameCanvas')
 const ctx = gameCanvas.getContext('2d')
+
+ctx.fillStyle = CANVAS_BACKGROUND_COLOR
+
+ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height)
